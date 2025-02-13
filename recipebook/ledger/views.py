0166c3402bd6569ddf011c559e4e3e7317534650
MoochieTenorio <jenuel.tenorio@student.ateneo.edu>
2025-02-13 21:23:35 +0800

    Add files via upload

---
+++ recipebook/ledger/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+
+recipes = [
+    {
+        "name": "Recipe 1",
+        "ingredients": [
+            {"name": "tomato", "quantity": "3pcs"},
+            {"name": "onion", "quantity": "1pc"},
+            {"name": "pork", "quantity": "1kg"},
+            {"name": "water", "quantity": "1L"},
+            {"name": "sinigang mix", "quantity": "1 packet"}
+        ],
+        "link": "/recipe/1"
+    },
+    {
+        "name": "Recipe 2",
+        "ingredients": [
+            {"name": "garlic", "quantity": "1 head"},
+            {"name": "onion", "quantity": "1pc"},
+            {"name": "vinegar", "quantity": "1/2cup"},
+            {"name": "water", "quantity": "1 cup"},
+            {"name": "salt", "quantity": "1 tablespoon"},
+            {"name": "whole black peppers", "quantity": "1 tablespoon"},
+            {"name": "pork", "quantity": "1 kilo"}
+        ],
+        "link": "/recipe/2"
+    }
+]
+
+def recipe_list(request):
+    return render(request, 'recipes_list.html', {"recipes": recipes})
+
+def recipe_detail_1(request):
+    return render(request, 'recipe_detail.html', {"recipe": recipes[0]})
+
+def recipe_detail_2(request):
+    return render(request, 'recipe_detail.html', {"recipe": recipes[1]})
