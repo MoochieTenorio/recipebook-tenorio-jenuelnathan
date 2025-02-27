89321e48c0acf02a08698fe0cc696ce5e002e54b
MoochieTenorio <jenuel.tenorio@student.ateneo.edu>
2025-02-27 23:49:47 +0800

    Added models, views, templates, and admin panel for lab2

---
+++ ledger/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f"/recipes/{self.id}/"
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="ingredients")
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="recipe")
+    quantity = models.CharField(max_length=100)  # Can store values like "1 cup", "2 tsp", etc.
+
+    def __str__(self):
+        return f"{self.quantity} of {self.ingredient.name} in {self.recipe.name}"
