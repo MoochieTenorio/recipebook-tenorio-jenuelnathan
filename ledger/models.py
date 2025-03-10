3920d2b80e2a2c52516aa80272317aaa1ca8633a
MoochieTenorio <jenuel.tenorio@student.ateneo.edu>
2025-03-11 05:42:55 +0800

    Updated Recipe model to include author, created_on, and updated_on fields

--- ledger/models.py
+++ ledger/models.py
@@ -11,12 +11,16 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipes")
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return f"/recipes/{self.id}/"
+        return reverse('recipe_detail', args=[str(self.id)])
+
 
 
 class RecipeIngredient(models.Model):
