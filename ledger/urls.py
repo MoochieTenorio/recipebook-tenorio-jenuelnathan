61457db5926de75b7e04bbb6e46eac4b260f430a
MoochieTenorio <jenuel.tenorio@student.ateneo.edu>
2025-03-10 23:39:12 +0800

    Updated URLs to include login, logout, and profile

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .views import recipe_list, recipe_detail
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import recipe_list, recipe_detail, custom_login, profile_view
 
 urlpatterns = [
     path("recipes/list", recipe_list, name="recipe_list"),
     path("recipe/<int:recipe_id>", recipe_detail, name="recipe_detail"),
+
+    # Authentication Routes
+    path("login/", custom_login, name="login"),
+    path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
+
+    # Profile Route (optional)
+    path("profile/", profile_view, name="profile"),
 ]
