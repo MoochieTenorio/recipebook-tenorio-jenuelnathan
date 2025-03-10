4d7713772e6437962cd2ffd5bd94edb388fb50b6
MoochieTenorio <jenuel.tenorio@student.ateneo.edu>
2025-03-10 23:30:50 +0800

    Added Profile model and updated Recipe model with author and timestamps

--- ledger/models.py
+++ ledger/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    short_bio = models.TextField()  
+
+    def __str__(self):
+        return self.name
 
 
 class Ingredient(models.Model):
@@ -10,6 +20,10 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) 
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return self.name
