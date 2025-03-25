9f7bfd92b83fe7a37a5e5d3f1622224876f5dcff
MoochieTenorio <jenuel.tenorio@student.ateneo.edu>
2025-03-25 18:46:12 +0800

    Modified Django admin to show Recipe Images inline

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, Recipe, RecipeImage
 
 
 class ProfileInline(admin.StackedInline):
@@ -13,5 +13,17 @@ class UserAdmin(BaseUserAdmin):
     inlines = [ProfileInline]
 
 
+class RecipeImageInline(admin.TabularInline):  # Shows RecipeImages inline in RecipeAdmin
+    model = RecipeImage
+    extra = 1  # Allows adding images directly from the Recipe page
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeImageInline]  # Attach RecipeImage inline to RecipeAdmin
+
+
+# Register models in Django admin
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+admin.site.register(Recipe, RecipeAdmin)  # Register Recipe with inline images
+admin.site.register(RecipeImage)  # Register RecipeImage separately
